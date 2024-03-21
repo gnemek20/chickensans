f801c0f76df4184fe062561d7ec3c083f05de8ba
gnemek20 <gnemek20@gmail.com>
2024-03-22 07:41:16 +0900

    타이틀 변경, 지도 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>DY ING</title>
+      </Head>
+      <Script
+        type="text/javascript"
+        strategy="beforeInteractive"
+        src="https://openapi.amp.naver.com/openapi/v3/maps.js?ncpClientId=6kbpgfbf8d"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
--- sections/map.tsx
+++ sections/map.tsx
@@ -1,7 +1,52 @@
+import { Background, Content, ContentChild } from "@/components";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import { useEffect, useRef } from "react";
+
 const map = () => {
+  interface imageProps {
+    src: StaticImport,
+    alt: string
+  }
+
+  const mapRef = useRef<HTMLElement | any>(null);
+
+  const backgroundImage: imageProps = {
+    src: require('@/public/images/tshirt.jpg'),
+    alt: 'backgroundImage'
+  }
+
+  useEffect(() => {
+    const dy_location: { latitude: number, longtitude: number } = {
+      latitude: 37.57360,
+      longtitude: 127.00450
+    }
+
+    mapRef.current = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
+      zoomControl: true,
+      zoom: 15
+    });
+    new naver.maps.Marker({
+      position: new naver.maps.LatLng(dy_location.latitude, dy_location.longtitude),
+      map: mapRef.current
+    });
+  }, []);
+
   return (
     <>
-      map section
+      <Background
+        src={backgroundImage.src}
+        alt={backgroundImage.alt}
+      ></Background>
+      <Content>
+        <ContentChild>
+          <h1>이곳에서</h1>
+          <h1>만들어요</h1>
+        </ContentChild>
+        <ContentChild>
+          <div id="map"></div>
+        </ContentChild>
+      </Content>
     </>
   )
 }
