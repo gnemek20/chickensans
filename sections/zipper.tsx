47cf78eb37feb850f479c22438da73c851549d59
gnemek20 <gnemek20@gmail.com>
2024-03-30 09:19:01 +0900

    마스크 있을 경우 메인 컨텐츠에 블러 효과 추가, 상품 강조 문구 추가

--- sections/zipper.tsx
+++ sections/zipper.tsx
@@ -1,3 +1,4 @@
+import { EmphasizeScroll } from "@/components";
 import style from "@/styles/sections/zipper.module.css";
 import { useEffect, useRef } from "react";
 
@@ -8,12 +9,14 @@ interface componentProps {
 const zipper = (props: componentProps) => {
   const maskRef = useRef<HTMLDivElement>(null);
   const maskAnimationRef = useRef<HTMLDivElement>(null);
+  const blurRef = useRef<HTMLDivElement>(null);
 
   const deleteMask = () => {
     setTimeout(maskFadeOut, 650);
   }
   const maskFadeOut = () => {
     maskRef.current?.classList.toggle(style.maskFadeOut);
+    blurRef.current?.classList.toggle(style.blur);
   }
 
   useEffect(() => {
@@ -32,6 +35,15 @@ const zipper = (props: componentProps) => {
           </div>
         </div>
       </div>
+      <div className={style.background}>
+        <div className={style.blur} ref={blurRef}>
+          <div className={style.emphasizeProduct}>
+            <h1>Long Lasting Best soft Zipper</h1>
+            <p>오래도록 부드러운 최고의 지퍼를 제공합니다.</p>
+          </div>
+        </div>
+      </div>
+      <EmphasizeScroll></EmphasizeScroll>
     </>
   )
 }
